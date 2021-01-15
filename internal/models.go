baf6d6743c3008073854440af6b80872f82b66ac
Ben Braunstein <BenZBraunstein@gmail.com>
2021-01-14 21:48:05 -0500

    added fields to structs

--- internal/models.go
+++ internal/models.go
@@ -5,6 +5,7 @@ import (
 	"github.com/BenBraunstein/haftr-alumni-golang/common/uuid"
 )
 
+// User is the internal representation of a user
 type User struct {
 	ID                   uuid.V4    `bson:"id"`
 	Email                string     `bson:"email"`
@@ -15,5 +16,68 @@ type User struct {
 	LastUpdatedTimestamp time.Epoch `bson:"lastUpdatedTimestamp"`
 }
 
+// Alumni is the internal representation of an Alumni
 type Alumni struct {
+	UserID                uuid.V4    `bson:"id"`
+	Firstname             string     `bson:"firstname"`
+	Middlename            string     `bson:"middlename"`
+	Lastname              string     `bson:"lastname"`
+	MarriedName           string     `bson:"marriedName"`
+	MotherName            string     `bson:"motherName"`
+	FatherName            string     `bson:"fatherName"`
+	CurrentAddress        string     `bson:"address"`
+	HomePhone             string     `bson:"homePhone"`
+	CellPhone             string     `bson:"cellPhone"`
+	WorkPhone             string     `bson:"workPhone"`
+	EmailAddress          string     `bson:"emailAddress"`
+	LastYearAttended      string     `bson:"lastYearAttended"`
+	IsraelSchool          string     `bson:"israelSchool"`
+	CollegeAttended       string     `bson:"collegeAttended"`
+	GradSchool            string     `bson:"gradSchool"`
+	Profession            string     `bson:"profession"`
+	Birthday              string     `bson:"birthday"`
+	Clubs                 []string   `bson:"clubs"`
+	SportsTeams           []string   `bson:"sportsTeams"`
+	Awards                []string   `bson:"awards"`
+	Committees            []string   `bson:"committees"`
+	OldAddresses          []string   `bson:"oldAddresses"`
+	HillelDayCamp         Camp       `bson:"hillelDayCamp"`
+	HillelSleepCamp       Camp       `bson:"hillelSleepCamp"`
+	HiliDayCamp           Camp       `bson:"hiliDayCamp"`
+	HiliWhiteCamp         Camp       `bson:"hiliWhiteCamp"`
+	HiliInternationalCamp Camp       `bson:"hiliInternationalCamp"`
+	HILI                  bool       `bson:"hili"`
+	HILLEL                bool       `bson:"hillel"`
+	HAFTR                 bool       `bson:"haftr"`
+	ParentOfStudent       bool       `bson:"parentOfStudent"`
+	Boards                []string   `bson:"boards"`
+	AlumniPositions       []string   `bson:"alumniPositions"`
+	Siblings              []Sibling  `bson:"siblings"`
+	Children              []Child    `bson:"children"`
+	ProfilePictureKey     string     `bson:"profilePictureKey"`
+	CreatedTimestamp      time.Epoch `bson:"createdTimestamp"`
+	LastUpdatedTimestamp  time.Epoch `bson:"lastUpdatedTimestamp"`
+}
+
+// Camp is the internal representation of a camp
+type Camp struct {
+	Attended  bool   `bson:"attended"`
+	StartYear string `bson:"startYear"`
+	EndYear   string `bson:"endYear"`
+	Specialty string `bson:"specialty"`
+	Camper    bool   `bson:"camper"`
+	Counselor bool   `bson:"counselor"`
+}
+
+// Sibling is the internal representation of a Sibling
+type Sibling struct {
+	Name          string `bson:"name"`
+	YearCompleted string `bson:"yearCompleted"`
+	School        string `bson:"school"`
+}
+
+// Child is the internal representation of a Child
+type Child struct {
+	Name           string `bson:"name"`
+	GraduationYear string `bson:"graduationYear"`
 }
--- pkg/models.go
+++ pkg/models.go
@@ -25,6 +25,68 @@ type User struct {
 	LastUpdatedTimestamp time.ISO8601 `json:"lastUpdatedTimestamp"`
 }
 
-// Alumni is the representation of a DTO alumni
+// Alumni is the DTO representation of an Alumni
 type Alumni struct {
+	UserID                uuid.V4    `json:"id"`
+	Firstname             string     `json:"firstname"`
+	Middlename            string     `json:"middlename"`
+	Lastname              string     `json:"lastname"`
+	MarriedName           string     `json:"marriedName"`
+	MotherName            string     `json:"motherName"`
+	FatherName            string     `json:"fatherName"`
+	CurrentAddress        string     `json:"address"`
+	HomePhone             string     `json:"homePhone"`
+	CellPhone             string     `json:"cellPhone"`
+	WorkPhone             string     `json:"workPhone"`
+	EmailAddress          string     `json:"emailAddress"`
+	LastYearAttended      string     `json:"lastYearAttended"`
+	IsraelSchool          string     `json:"israelSchool"`
+	CollegeAttended       string     `json:"collegeAttended"`
+	GradSchool            string     `json:"gradSchool"`
+	Profession            string     `json:"profession"`
+	Birthday              string     `json:"birthday"`
+	Clubs                 []string   `json:"clubs"`
+	SportsTeams           []string   `json:"sportsTeams"`
+	Awards                []string   `json:"awards"`
+	Committees            []string   `json:"committees"`
+	OldAddresses          []string   `json:"oldAddresses"`
+	HillelDayCamp         Camp       `json:"hillelDayCamp"`
+	HillelSleepCamp       Camp       `json:"hillelSleepCamp"`
+	HiliDayCamp           Camp       `json:"hiliDayCamp"`
+	HiliWhiteCamp         Camp       `json:"hiliWhiteCamp"`
+	HiliInternationalCamp Camp       `json:"hiliInternationalCamp"`
+	HILI                  bool       `json:"hili"`
+	HILLEL                bool       `json:"hillel"`
+	HAFTR                 bool       `json:"haftr"`
+	ParentOfStudent       bool       `json:"parentOfStudent"`
+	Boards                []string   `json:"boards"`
+	AlumniPositions       []string   `json:"alumniPositions"`
+	Siblings              []Sibling  `json:"siblings"`
+	Children              []Child    `json:"children"`
+	ProfilePictureKey     string     `json:"profilePictureKey"`
+	CreatedTimestamp      time.Epoch `json:"createdTimestamp"`
+	LastUpdatedTimestamp  time.Epoch `json:"lastUpdatedTimestamp"`
+}
+
+// Camp is the DTO representation of a camp
+type Camp struct {
+	Attended  bool   `json:"attended"`
+	StartYear string `json:"startYear"`
+	EndYear   string `json:"endYear"`
+	Specialty string `json:"specialty"`
+	Camper    bool   `json:"camper"`
+	Counselor bool   `json:"counselor"`
+}
+
+// Sibling is the DTO representation of a Sibling
+type Sibling struct {
+	Name          string `json:"name"`
+	YearCompleted string `json:"yearCompleted"`
+	School        string `json:"school"`
+}
+
+// Child is the DTO representation of a Child
+type Child struct {
+	Name           string `json:"name"`
+	GraduationYear string `json:"graduationYear"`
 }
